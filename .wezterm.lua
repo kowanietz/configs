7fdb2cbc297d5e7d86e56da07626f3afdb674c29
Ben Kowanietz <109610315+kowanietz@users.noreply.github.com>
2023-05-15 22:57:27 +0200

    it had to be done

--- .wezterm.lua
+++ .wezterm.lua
@@ -15,7 +15,7 @@ config.integrated_title_button_alignment = "Right"
 config.initial_cols = 90
 config.initial_rows = 30
 
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.85
 
 config.font = wezterm.font_with_fallback {
   {
